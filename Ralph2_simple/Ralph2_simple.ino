61e3b85dc569ac7016a7db118694666054c438ce
finn42 <finnlattimore@gmail.com>
2016-02-27 13:54:52 +1100

    captitalize constants

--- Ralph2_simple/Ralph2_simple.ino
+++ Ralph2_simple/Ralph2_simple.ino
@@ -1,25 +1,25 @@
 
 #include <NewPing.h>
 
-const int trigger_pin = 14;
-const int echo_pin = 15;
-const int max_distance = 100; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-const int rr = 5;
-const int rf = 3;
-const int lr = 9;
-const int lf = 6;
+const int TRIGGER_PIN = 14;
+const int ECHO_PIN = 15;
+const int MAX_DISTANCE = 100; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+const int RR = 5;
+const int RF = 3;
+const int LR = 9;
+const int LF = 6;
+const int MAX_SPEED = 200;
+const int OK_DIST = 15; 
 
-int ok_dist = 15; 
-int max_speed = 200;
 
-NewPing sonar(trigger_pin, echo_pin, max_distance); // NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  pinMode(rf,OUTPUT);
-  pinMode(rr,OUTPUT);
-  pinMode(lf,OUTPUT);
-  pinMode(lr,OUTPUT);
+  pinMode(RF,OUTPUT);
+  pinMode(RR,OUTPUT);
+  pinMode(LF,OUTPUT);
+  pinMode(LR,OUTPUT);
 }
 
 void loop() {
@@ -28,23 +28,23 @@ void loop() {
   unsigned int microseconds = sonar.ping_median(5); // get median return time of 5 pings in microseconds (uS).
   int distance = sonar.convert_cm(microseconds); // convert microseconds to cm (based on speed of sound)
   if (distance == 0) { // zero is returned if no object in range.
-    distance = ok_dist;
+    distance = MAX_DISTANCE;
   }
    Serial.print("distance:");
    Serial.println(distance);  
-  if (distance < ok_dist) {
+  if (distance < OK_DIST) {
    Serial.println("turn turn turn"); 
-   analogWrite(lf,max_speed);
-   analogWrite(rr,max_speed);
-   analogWrite(lr,0);
-   analogWrite(rf,0);
+   analogWrite(LF,MAX_SPEED);
+   analogWrite(RR,MAX_SPEED);
+   analogWrite(LR,0);
+   analogWrite(RF,0);
    
   } else {
-    Serial.println("of we go");
-    analogWrite(lf,max_speed);
-    analogWrite(rf,max_speed);
-    analogWrite(rr,0);
-    analogWrite(lr,0);
+    Serial.println("off we go");
+    analogWrite(LF,MAX_SPEED);
+    analogWrite(RF,MAX_SPEED);
+    analogWrite(RR,0);
+    analogWrite(LR,0);
   }
   
 }
